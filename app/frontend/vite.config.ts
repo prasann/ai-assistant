3bb7dbab45888a24aaab594fff225bf9770a8333
Prasanna Nagarajan <mail.prasanna.v@gmail.com>
2025-01-08 14:21:11 +0530

    making this as pwa

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,10 +1,41 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
+            manifest: {
+                name: "Personal Assistant",
+                short_name: "PDA",
+                description: " A personal assistant that helps you with your daily tasks.",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "./assets/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "./assets/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    }
+                ]
+            }
+        }),
+        viteStaticCopy({
+            targets: [
+                { src: "src/assets/pwa-192x192.png", dest: "../../backend/static/assets" },
+                { src: "src/assets/pwa-512x512.png", dest: "../../backend/static/assets" }
+            ]
+        })
+    ],
     build: {
         outDir: "../backend/static",
         emptyOutDir: true,
